35be5cab16f09209d10829d7425e3666f14068cc
D-SUNYO-A <sdanarson1@gmail.com>
2024-05-14 19:08:48 +0300

    test

--- app/components/marketing/hero.tsx
+++ app/components/marketing/hero.tsx
@@ -15,7 +15,7 @@ export const Hero = () => {
       <div className="absolute inset-0 flex flex-col items-center justify-center gap-10 text-center">
         <img src={logo.url} alt="logo" className="w-40" />
         <div className="flex flex-col items-center justify-center gap-6 text-white">
-          <h2 className="text-4xl md:text-5xl font-lobster max-w-[855px]">Régalez-vous des meilleurs plats du monde à volonté avec Ramarie.</h2>
+          <h2 className="text-4xl md:text-5xl font-lobster max-w-[855px]">Hi Ramarie.</h2>
           <p className="text-lg md:text-xl">Petit déjeuner - Déjeuner - Dîner</p>
         </div>
         <div className="flex py-3 px-4 bg-background rounded-lg">
