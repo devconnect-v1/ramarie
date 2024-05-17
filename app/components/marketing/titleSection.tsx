6bb425b0da8e1c00103cdf5d07b98b2a10a32cdc
D-SUNYO-A <sdanarson1@gmail.com>
2024-05-17 15:52:49 +0300

    Contact Section done ✅

--- app/components/marketing/titleSection.tsx
+++ app/components/marketing/titleSection.tsx
@@ -1,15 +1,24 @@
 interface TitleSectionProps {
   subtitle: string;
   title: string;
+  subtitleColor?: string;
+  titleColor?: string;
+  lineColor?: string;
 }
 
-const TitleSection: React.FC<TitleSectionProps> = ({ subtitle, title }) => {
+const TitleSection: React.FC<TitleSectionProps> = ({
+  subtitle,
+  title,
+  subtitleColor,
+  titleColor,
+  lineColor,
+}) => {
   return (
     <div className="flex flex-col gap-0">
-      <span className="flex items-center gap-2 uppercase after:content-[''] after:w-24 after:h-[2px] after:bg-primary text-sm font-light text-muted-foreground">
+      <span className={`flex items-center gap-2 uppercase after:content-[''] after:w-24 after:h-[2px] ${lineColor ? lineColor : "after:bg-primary"} text-sm font-light ${subtitleColor ? subtitleColor : "text-muted-foreground"}`}>
         {subtitle}
       </span>
-      <h1 className="text-[60px] text-secondary-foreground font-heading font-light">
+      <h1 className={`text-[60px] ${titleColor ? titleColor : "text-secondary-foreground"} font-heading font-light`}>
         {title}
       </h1>
     </div>
